ae6f0c0c94a7a09a36734ec4831babcf029f0ac1
Ivan Trofimov <iam@ivan-trofimov.ru>
2019-09-20 16:01:29 +0700

    Создан таймер с секундомером, секундомер расширен до миллисекунд. Также предусмотрено сохранение параметров при повороте экрана (обработка перезапуска активности с прежними значениями таймера)

---
+++ app/src/main/java/com/example/timer/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.timer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    private int seconds = 0;
+    private boolean isRunning = false;
+
+    private TextView textViewTimer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textViewTimer = findViewById(R.id.textViewTimer);
+
+        if (savedInstanceState != null) {
+            seconds = savedInstanceState.getInt("seconds");
+            isRunning = savedInstanceState.getBoolean("isRunning");
+        }
+
+        runTimer();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("seconds", seconds);
+        outState.putBoolean("isRunning", isRunning);
+    }
+
+    public void onClickStartTimer(View view) {
+        isRunning = true;
+    }
+
+    public void onClickPauseTimer(View view) {
+        isRunning = false;
+    }
+
+    public void onClickStopTimer(View view) {
+        isRunning = false;
+        seconds = 0;
+    }
+
+    private  void runTimer(){
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/360000;
+                int minutes = (seconds % 360000)/6000;
+                int secs = (seconds % 6000) / 100;
+                int msecs = (seconds % 100);
+
+
+                String time = String.format(Locale.getDefault(), "%d:%02d:%02d:%02d", hours, minutes, secs, msecs);
+                textViewTimer.setText(time);
+
+                if(isRunning){
+                    seconds++;
+                }
+                handler.postDelayed(this, 10);
+            }
+        });
+    }
+}
